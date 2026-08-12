46fa908d28602acf7a763e3faf1cfffbf3e5e686
九九 <jiujiu@jiujiudeMacBook-Air.local>
2026-08-12 18:27:36 +0800

    Switch to independent Supabase project

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://srdsjhvrwbrabdwigexa.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNyZHNqaHZyd2JyYWJkd2lnZXhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2Nzk5MDIsImV4cCI6MjA5OTI1NTkwMn0.Vd6KjcVB9slZSz85_1ZE8w8lZCKr74rfStebIPhM8vc';
+const supabaseUrl = 'https://hjaaggkxvmutddtznmmc.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhqYWFnZ2t4dm11dGRkdHpubW1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY1MjU0MDYsImV4cCI6MjEwMjEwMTQwNn0.wctOdETBMjmOGJXYkFN6Cjt9AriKuBlz67q4fgHTNSs';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
